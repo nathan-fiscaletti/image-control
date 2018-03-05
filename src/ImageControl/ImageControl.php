b9754572592483073a1c196718f2c111fe5f2cd3
Nathan Fiscaletti <nathhan.fiscaletti@gmail.com>
2018-03-05 10:20:33 -0600

    Updated ImageControl.php

--- src/ImageControl/ImageControl.php
+++ src/ImageControl/ImageControl.php
@@ -16,32 +16,32 @@ class ImageControl
         try {
             $img = imagecreatefromstring(file_get_contents($_GET['image']));
         } catch (\Exception $e) {
-            $this->displayImage(null);
+            $img = null;
             return;
         }
 
-        if (empty($_GET['width']) || !is_numeric($_GET['width'])) {
-            $this->displayImage(null);
-            return;   
+        $width = null;
+        $height = null;
+
+        if (! empty($_GET['width']) && is_numeric($_GET['width'])) {
+            $width = $_GET['width'];
         }
 
-        if (empty($_GET['height']) || !is_numeric($_GET['height'])) {
-            $this->displayImage(null);
-            return;   
+        if (! empty($_GET['height']) && is_numeric($_GET['height'])) {
+            $height = $_GET['height'];
         }
 
-        $width = $_GET['width'];
-        $height = $_GET['height'];
-
-        $img = imagescale($img, $width, $height);
-
-        $this->displayImage($img);
+        $this->displayImage($img, $width, $height);
     }
 
-    private function displayImage($image)
+    private function displayImage($image, $width, $height)
     {
         if ($image === null) {
             $image = imagecreatefromstring(file_get_contents('./res/unknown.png'));
+        }
+
+        if ($width === null || $height === null) {
+            $image = imagescale($image, $width, $height)
         }
 
         header('Content-Type: image/png');
