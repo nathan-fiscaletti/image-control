003e49ef758795122e7801df10528ada37dee1b6
Nathan Fiscaletti <nathhan.fiscaletti@gmail.com>
2018-03-05 10:51:47 -0600

    Updated index.php

--- public/index.php
+++ public/index.php
@@ -1,59 +1,62 @@
 <html>
-    <h1>Image Control</h1>
-    <br />
+    <head>
+        <title>Image Control by -DI- MrFisc</title>
+    </head>
+    <body>
+        <h1>Image Control</h1>
+        <br />
 
-    <h2>Resize Image URL</h2>
-    <form action='./' method='POST'>
-        <input type='hidden' name='generate' value='1' />
-        <table>
-            <tr>
-                <td style='width: 100px'>Image URL:</td>
-                <td><input type='text' name='image' style='width:300px' /></td>
-            </tr>
-            <tr>
-                <td>Width:</td>
-                <td><input type='number' name='width' /> <b>px</b></td>
-            </tr>
-            <tr>
-                <td>Height:</td>
-                <td><input type='number' name='height' /> <b>px</b></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td><input type='submit' value='Create URL for resized image' /></td>
-            </tr>
-        </table>
-    </form>
-
-    <?php 
-    if (isset($_POST['generate'])) { 
-        $url = 'http://image-control.online/image.php?image='.$_POST['image'].'&width='.$_POST['width'].'&height='.$_POST['height'];
-
-        ?>
-            <h2>Result</h2>
-
+        <h2>Resize Image URL</h2>
+        <form action='./' method='POST'>
+            <input type='hidden' name='generate' value='1' />
             <table>
                 <tr>
-                    <td>
-                        <b>URL: </b>
-                    </td>
-                    <td>
-                        <a href='<?php echo $url; ?>'><?php echo $url; ?></a>
-                    </td>
+                    <td style='width: 100px'>Image URL:</td>
+                    <td><input type='text' name='image' style='width:300px' /></td>
                 </tr>
                 <tr>
-                    <td valign='top'>
-                        <b>Preview: </b>
-                    </td>
-                    <td>
-                        <img src='<?php echo $url; ?>' />
-                    </td>
+                    <td>Width:</td>
+                    <td><input type='number' name='width' /> <b>px</b></td>
+                </tr>
+                <tr>
+                    <td>Height:</td>
+                    <td><input type='number' name='height' /> <b>px</b></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><input type='submit' value='Create URL for resized image' /></td>
                 </tr>
             </table>
-        <?php
-    }
-    ?>
+        </form>
 
+        <?php 
+        if (isset($_POST['generate'])) { 
+            $url = 'http://image-control.online/image.php?image='.$_POST['image'].'&width='.$_POST['width'].'&height='.$_POST['height'];
 
+            ?>
+                <h2>Result</h2>
+
+                <table>
+                    <tr>
+                        <td>
+                            <b>URL: </b>
+                        </td>
+                        <td>
+                            <a href='<?php echo $url; ?>'><?php echo $url; ?></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td valign='top'>
+                            <b>Preview: </b>
+                        </td>
+                        <td>
+                            <img src='<?php echo $url; ?>' />
+                        </td>
+                    </tr>
+                </table>
+            <?php
+        }
+        ?>
+    </body>
 </html>
 
