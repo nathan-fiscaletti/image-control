531fe105a42f764ee4aebda376d42179895b62b3
Nathan Fiscaletti <nathan.fiscaletti@gmail.com>
2018-03-06 12:31:07 -0600

    Updated index.php

--- public/index.php
+++ public/index.php
@@ -12,14 +12,13 @@
         <!-- Place this tag in your head or just before your close body tag. -->
         <script async defer src="https://buttons.github.io/buttons.js"></script>
     </head>
-    <body >
-        <a href="https://github.com/nathan-fiscaletti/image-control"><img style="position: absolute; top: 0; right: 0; border: 0; z-index: 2147483647;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png" alt="Fork me on GitHub"></a>
+    <body style='background:#eee;'>
         <div class="container">
             <div class="row">
-                <div class="col-md-6 col-md-offset-3">
-                    <div align='center'>
-                        <h1>Image Control</h1>
-                    </div>
+                <br />
+                <div class="col-md-6 col-md-offset-3" style='background: #fff;box-shadow: 0 0 15px rgba(0,0,0,0.1);border: 1px solid #ccc;padding:40px;'>
+                    <a href="https://github.com/nathan-fiscaletti/image-control"><img style="position: absolute; top: 0; right: 0; border: 0; z-index: 2147483647;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png" alt="Fork me on GitHub"></a>
+                    <h1 style='color: #444;font-size: 36px;font-family: sans-serif;font-weight: bold;'>Image Control</h1>
                     <hr />
                     <h3>Generate Re-Size Image URL</h3> 
                     <p> Enter an Image URL and a desired width and height and we will generate a re-size wrapper URL for the image. </p>
@@ -69,6 +68,32 @@
                         <a class="github-button" href="https://github.com/nathan-fiscaletti" aria-label="Follow @nathan-fiscaletti on GitHub">Follow @nathan-fiscaletti</a>
                         <br /><br />
                         <p style='font-size: 12px;font-style: italic;'>© 2018 Nathan Fiscaletti</p>
+                        <!-- Start of Globel Code -->
+                        <CENTER style='display:none;'>
+                        <script language="JavaScript">
+                            var count = "imagecontrol";          // Change Your Account?
+                            var type = "smallblack";       // Change Your Counter Image?
+                            var digits = "7";          // Change The Amount of Digits on Your Counter?
+                            var prog = "hit";          // Change to Either hit/unique?
+                            var statslink = "no";    // provide statistical link in counter yes/no?
+                            var sitelink = "yes";     // provide link back to our site;~) yes/no?
+                            var cntvisible = "yes"; // do you want counter visible yes/no?
+                        </script>
+                        <!-- START DO NOT TAMPER WITH ANYTHING ELSE BELOW THIS LINE FOR YOUR WEBTV & UNIX VISITORS -->
+                        <script language="JavaScript" src="http://006.free-counters.co.uk/count-123.js">
+                        </script>
+                        <noscript>
+                        <a href="http://www.free-counters.co.uk" target="_blank">
+                        <img  src="http://006.free-counters.co.uk/count-123.pl?count=imagecontrol&cntvisible=no&mode=noscript" alt="Free Counters" title="Free Counters" border="0"/>
+                        </a>The following text will not be seen after you upload your website,
+                        please keep it in order to retain your counter functionality 
+                        <br><a href="http://www.free-counters.co.uk" target="_blank">Free Trackers</a><br> <a href="http://www.free-counters.co.uk" target="_blank">Help</a><br>
+
+                        </noscript>
+                        <!-- END DO NOT TAMPER WITH ANYTHING ELSE ABOVE THIS LINE FOR YOUR WEBTV & UNIX VISITORS -->
+                        </CENTER>        
+                        <!-- End of Globel Code -->
+
                     </div>
                 </div>
             </div>
